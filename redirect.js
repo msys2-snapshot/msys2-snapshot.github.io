67442861806eb91cb3350eabaca73aaf74483eab
Алексей Костюнин <72197355+thirdpartyman@users.noreply.github.com>
2024-07-25 15:15:38 +0300

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,8 +1,5 @@
 var url = location.href;
-alert(url);
 url1 = url.replace("https://msys2-snapshot.github.io/msys2?", "");
-alert(url1);
 url = url.replace("https://msys2-snapshot.github.io/msys2?", "https://downloads.sourceforge.net/project/msys2-snapshot/files/msys2/");
-alert(url);
 location.href = url;
 
