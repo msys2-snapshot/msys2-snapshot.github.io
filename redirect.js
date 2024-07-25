9f48f594194ba167b05544fbd473d91a0f373812
Алексей Костюнин <72197355+thirdpartyman@users.noreply.github.com>
2024-07-25 14:39:00 +0300

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,6 +1,6 @@
 var url = location.href;
 alert(url);
-url = url.replace("https://msys2-snapshot.github.io/", "https://downloads.sourceforge.net/project/msys2-snapshot/files/");
+url = url.replace("https://msys2-snapshot.github.io/msys2?", "https://downloads.sourceforge.net/project/msys2-snapshot/files/msys2");
 alert(url);
 location.href = url;
 
