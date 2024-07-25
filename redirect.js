9c4fb32744b67e2b5592d050e6395538a67b9dcc
Алексей Костюнин <72197355+thirdpartyman@users.noreply.github.com>
2024-07-25 14:21:31 +0300

    so hard...

--- redirect.js
+++ redirect.js
@@ -1,3 +1,3 @@
 var url = location.href;
-url = url.replace("http://msys2-snapshot.github.io/", "http://downloads.sourceforge.net/project/msys2-snapshot/files/");
+url = url.replace("https://msys2-snapshot.github.io/", "http://downloads.sourceforge.net/project/msys2-snapshot/files/");
 location.href = url;
